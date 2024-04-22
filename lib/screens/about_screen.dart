717890f99be19c74310fe4294bd3eb8a4da752e1
Juan Sierra <juansiediaz97@gmail.com>
2024-04-22 08:46:33 -0700

    (feat): Updated the about screen.

--- lib/screens/about_screen.dart
+++ lib/screens/about_screen.dart
@@ -4,8 +4,69 @@ class AboutScreen extends StatelessWidget {
   const AboutScreen({Key? key}) : super(key: key);
   @override
   Widget build(BuildContext context) {
-    return const Center(
-      child: Text('About Screen'),
+    return Scaffold(
+      backgroundColor: Theme.of(context).colorScheme.primary,
+      body: Container(
+        constraints: const BoxConstraints.expand(),
+        padding: const EdgeInsets.all(16),
+        child: InkWell(
+          onTap: () {},
+          child: Ink(
+              decoration: BoxDecoration(
+                color: Theme.of(context).colorScheme.secondary.withOpacity(0.5),
+                borderRadius: BorderRadius.circular(8),
+              ),
+              child: const Padding(
+                padding: EdgeInsets.all(8.0),
+                child: Column(
+                  crossAxisAlignment: CrossAxisAlignment.center,
+                  children: [
+                    Text(
+                      'Weather App ',
+                      style: TextStyle(
+                        fontSize: 24,
+                      ),
+                    ),
+                    SizedBox(height: 16),
+                    Text(
+                      'This is a weather forecasting app that provides real-time weather updates. '
+                      'It fetches data from the OpenWeather API and presents it in a user-friendly interface. ',
+                      textAlign: TextAlign.center,
+                      style: TextStyle(
+                        fontSize: 18,
+                      ),
+                    ),
+                    SizedBox(height: 16),
+                    Text(
+                      'OpenWeatherMap API is a service that provides weather data, including current weather data, forecasts, and historical data to the developers of web services and mobile applications. It provides an API with JSON, XML and HTML endpoints and a limited free service.',
+                      textAlign: TextAlign.center,
+                      style: TextStyle(
+                        fontSize: 18,
+                      ),
+                    ),
+                    SizedBox(height: 16),
+                    Text(
+                      'This app was created using Flutter. '
+                      'Flutter is an open-source UI software development kit created by Google. '
+                      'It is used to develop applications for Android, iOS, Linux, Mac, Windows, Google Fuchsia, and the web from a single codebase.',
+                      textAlign: TextAlign.center,
+                      style: TextStyle(
+                        fontSize: 18,
+                      ),
+                    ),
+                    SizedBox(height: 16),
+                    Text(
+                      'Developed by: Juan Pablo Sierra Diaz',
+                      textAlign: TextAlign.center,
+                      style: TextStyle(
+                        fontSize: 18,
+                      ),
+                    ),
+                  ],
+                ),
+              )),
+        ),
+      ),
     );
   }
 }
