f991a18fc50de936768c2f0dbabf537d80020ae1
Juan Pablo Sierra Diaz <89560629+jsierradiaz@users.noreply.github.com>
2024-04-22 08:46:59 -0700

    (feat): Updated the about screen. (#7)

--- lib/screens/settings_screen.dart
+++ lib/screens/settings_screen.dart
@@ -5,8 +5,41 @@ class SettingsScreen extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return const Center(
-      child: Text('Settings Screen'),
+    return Scaffold(
+      backgroundColor: Theme.of(context).colorScheme.primary,
+      body: const Center(
+        child: Text(
+          'Settings',
+          style: TextStyle(
+            fontSize: 24,
+            color: Colors.white,
+          ),
+        ),
+      ),
+      // body: InkWell(
+      //   onTap: () {
+      //     // Navigator.of(context).pop();
+      //   },
+      //   child: Card(
+      //     margin: const EdgeInsets.all(16),
+      //     child: Column(
+      //       children: const [
+      //         ListTile(
+      //           title: Text('Temperature Units'),
+      //           trailing: Text('Celsius'),
+      //         ),
+      //         ListTile(
+      //           title: Text('Wind Speed Units'),
+      //           trailing: Text('km/h'),
+      //         ),
+      //         ListTile(
+      //           title: Text('Pressure Units'),
+      //           trailing: Text('hPa'),
+      //         ),
+      //       ],
+      //     ),
+      //   ),
+      // ),
     );
   }
 }
